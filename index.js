61ed5b717c4f3c9cbdf1aa03e0613d1f38656055
DiorAbjalilov <abjalilovdiyor@gmail.com>
2021-12-20 15:06:21 +0500

    asd

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", () => {
-  console.log("Hello Express Js");
+const path = require("path");
+
+app.get("/", (req, res) => {
+  //   console.log("Hello Express Js");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.listen(5000, () => console.log("Server run..."));
+app.listen(5000, () => console.log("Server is run..."));
