d9578c29f7b31746e4814fa39cbd496b902a2e07
DiorAbjalilov <abjalilovdiyor@gmail.com>
2021-12-20 17:31:27 +0500

    18-dars

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
-
 app.set("view engine", "ejs");
+const userRouter = require("./routes/user");
 
 app.get("/", (req, res) => {
   res.render("index", { title: "World" });
 });
-app.get("/about", (req, res) => {
-  res.render("index", { title: "About" });
-});
-app.get("/contact", (req, res) => {
-  res.render("index", { title: "Contact" });
-});
+app.use("/user/", userRouter);
+
 app.listen(5000, () => console.log("Server is run..."));
